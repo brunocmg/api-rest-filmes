e4dc2fbf08ff1e515e2604d3e6634e5a695166dc
Willian Figueiredo <willianfigueiredodev@gmail.com>
2025-10-08 15:39:51 -0300

    feat: implements movie listing (GET /movies)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { findAll as findAllMovies} from './controllers/movie.controller';
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.get('/movies', findAllMovies);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
