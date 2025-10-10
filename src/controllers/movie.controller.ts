439ef4c743e60cbc741b43cc31277b2b53d99a8a
brunocmg <brunocmg2006@gmail.com>
2025-10-10 12:06:56 -0300

    feat: add GET /movies/:id handler with UUID validation

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { movieService } from '../service/movie.service';
+import { validate as isUuid } from "uuid";
 
 export const findAll = (req: Request, res: Response) => {
   const movies = movieService.findAll();
@@ -17,4 +18,22 @@ export const create = (req: Request, res: Response) => {
   const newMovie = movieService.create(name, genre, year);
 
   return res.status(201).json(newMovie);
+}
+
+export const findById = (req: Request, res: Response) => {
+  const id = req.params.id
+  if (!id) {
+    return res.status(400).json({ message: 'Param "id" is required.' });
+  }
+
+  if (!isUuid(id))
+    return res.status(400).json({ message: "Invalid id format" });
+
+  const movie = movieService.findById(id);
+
+  if (!movie) {
+    return res.status(404).json({ message: `Movie with id ${id} not found.` });
+  }
+
+  return res.status(200).json(movie);
 }
