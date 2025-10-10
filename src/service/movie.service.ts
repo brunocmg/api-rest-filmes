11e9f97f4bdac4c820148d26e19a5cc47fee8c5a
brunocmg <brunocmg2006@gmail.com>
2025-10-10 13:30:07 -0300

    feat: add update(id) for PUT /movies/:id

--- src/service/movie.service.ts
+++ src/service/movie.service.ts
@@ -43,6 +43,23 @@ class MovieService {
   public findById(id: string): Movie | undefined {
     return this.movies.find((m) => m.id === id);
   }
+
+  public update(
+    id: string,
+    dto: { name: string; genre: string; year: number }
+  ): Movie | undefined {
+    const index = this.movies.findIndex((m) => m.id === id);
+    if (index === -1) return undefined;
+
+    const updated: Movie = {
+      id,
+      name: dto.name,
+      genre: dto.genre,
+      year: dto.year,
+    };
+    this.movies[index] = updated;
+    return updated;
+  }
 }
 
 export const movieService = new MovieService();
