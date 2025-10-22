d0fe4d4c0d5470b0179796811795db8456b500a1
brunocmg <brunocmg2006@gmail.com>
2025-10-22 18:18:22 -0300

    refactor: remove deprecated in-memory movie array

--- src/service/movie.service.ts
+++ src/service/movie.service.ts
@@ -2,27 +2,6 @@ import { deleteAllMovies } from "../controllers/movie.controller";
 import { Movie } from "../models/movie.model";
 
 class MovieService {
-  private movies: Movie[] = [
-    {
-      id: "6f709089-b430-4595-b286-8d49b5e5233d",
-      name: "Vingadores: Ultimato",
-      genre: "Ação/Aventura",
-      year: 2019,
-    },
-    {
-      id: "32599e52-c651-4861-a035-7142b694c25f",
-      name: "O Poderoso Chefão",
-      genre: "Drama/Crime",
-      year: 1972,
-    },
-    {
-      id: "c865764d-5334-4513-8b7a-62425c277e90",
-      name: "Interestelar",
-      genre: "Ficção Científica/Drama",
-      year: 2014,
-    },
-  ];
-
   public create(name: string, genre: string, year: number): Movie {
     const id = crypto.randomUUID();
     const newMovie: Movie = {
