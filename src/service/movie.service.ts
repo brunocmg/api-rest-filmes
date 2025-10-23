b2787f441831637a30006acd54db8e2a5f9f5c56
brunocmg <brunocmg2006@gmail.com>
2025-10-23 17:03:22 -0300

    refactor: changed the return of the delete movie

--- src/service/movie.service.ts
+++ src/service/movie.service.ts
@@ -1,5 +1,3 @@
-// import { deleteAllMovies } from "../controllers/movie.controller";
-import { Movie } from "../models/movie.model";
 import { PrismaClient } from "../generated/prisma";
 import { prisma } from "../lib/prisma";
 
@@ -59,8 +57,7 @@ class MovieService {
     const index = await prisma.movie.findUnique({ where: { id: id } });
     if (!index) return undefined;
 
-    const removed = prisma.movie.delete({where: {id: index.id}});
-    return removed;
+    return await prisma.movie.delete({where: {id: index.id}});
   }
 
   public async deleteAllMovies() {
